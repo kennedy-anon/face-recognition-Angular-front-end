4a7b03defe5742070426137cda890fecc5c169b5
kennedy-anon <85305895+kennedy-anon@users.noreply.github.com>
2023-02-23 16:43:58 +0300

    working on search face page

--- src/app/components/search-face/search-face.component.ts
+++ src/app/components/search-face/search-face.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+class ImageSnippet {
+  constructor(public src: string, public file: File) {}
+}
+
 @Component({
   selector: 'app-search-face',
   templateUrl: './search-face.component.html',
   styleUrls: ['./search-face.component.css']
 })
 export class SearchFaceComponent {
+  selectedImage !: ImageSnippet;
+  faceImage !: File;
+
+  // for processing face image
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.addEventListener('load', (event: any)=>{
+      this.selectedImage = new ImageSnippet(event.target.result, file);
+      this.faceImage = this.selectedImage.file;
+    })
+
+    reader.readAsDataURL(file);
+
+  }
 
 }
