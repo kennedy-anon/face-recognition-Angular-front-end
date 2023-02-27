034c13afa80acb5bc29284b4242230c29d848a2f
kennedy-anon <85305895+kennedy-anon@users.noreply.github.com>
2023-02-27 14:14:24 +0300

    train model page design

--- src/app/components/train-model/train-model.component.ts
+++ src/app/components/train-model/train-model.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
+class ImageSnippet {
+  constructor(public src: string, public file: File) {}
+}
+
 @Component({
   selector: 'app-train-model',
   templateUrl: './train-model.component.html',
   styleUrls: ['./train-model.component.css']
 })
 export class TrainModelComponent {
+  selectedImages !: ImageSnippet[];
+  faceImages !: FileList;
 
+
+    // for processing selected training face images
+    onFileSelected(event: any) {
+      this.selectedImages = [];
+      this.faceImages = event.target.files
+      
+      if (this.faceImages){
+        for (let i=0; i < 3; i++){
+          const reader = new FileReader();
+
+          reader.onload = (event: any) => {
+            const imageSrc = event.target.result;
+            const selectedImage = new ImageSnippet(imageSrc, this.faceImages[i]);
+            this.selectedImages.push(selectedImage);
+          };
+
+          reader.readAsDataURL(this.faceImages[i])
+        }
+      }
+  
+    }
 }
