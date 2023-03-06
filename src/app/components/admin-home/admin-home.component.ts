999fdca150ad362e4b9d317f4b64dfcb179f30f4
kennedy-anon <85305895+kennedy-anon@users.noreply.github.com>
2023-03-06 17:59:24 +0300

    logs for home page

--- src/app/components/admin-home/admin-home.component.ts
+++ src/app/components/admin-home/admin-home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SearchFaceLogsService } from 'src/app/services/search-face-logs.service';
 
 @Component({
   selector: 'app-admin-home',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-home.component.css']
 })
 export class AdminHomeComponent {
+  displayedColumns: string[] = ['log_id', 'crime_officer_id', 'crime_officer_name', 'face_id', 'face_name', 'search_date']; // for displaying logs
+  logsHistory : any;
 
+  constructor(private faceSearchLogService: SearchFaceLogsService) {}
+
+  //fetchLogs
+  fetchLogs() {
+    const access_token = localStorage.getItem('access');
+
+    if (access_token) {
+      this.faceSearchLogService.retrieveLogs(access_token, '')
+      .subscribe(res => {
+        this.logsHistory = res;
+      })
+    }
+  }
+
+  ngOnInit(): void {
+    this.fetchLogs();
+  }
 }
